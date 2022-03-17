d7c36d6cf0b12deec7946af3e7375031818a2946
SOLANKIJAY15 <jaysolanki934@gmail.com>
2022-03-17 12:27:31 +0530

    add files

--- application/views/footer.php
+++ application/views/footer.php
@@ -28,7 +28,7 @@
                             <a href="https://www.facebook.com/login/"><i class="fab fa-facebook-f"></i></a>
                             <a href="https://www.linkedin.com/login"><i class="fab fa-linkedin-in"></i></a>
                             <a href="https://www.instagram.com/accounts/login/"><i class="fab fa-instagram"></i></a>
-                            <a href="https://www.instagram.com/accounts/login/"><i class="fab fa-instagram"></i></a>
+                           
                             <a href="https://accounts.google.com/signin/v2/identifier?service=youtube&passive=1209600&continue=https%3A%2F%2Fstudio.youtube.com%2F&followup=https%3A%2F%2Fstudio.youtube.com%2F&flowName=GlifWebSignIn&flowEntry=ServiceLogin"><i class="fab fa-youtube"></i></a>
                         </div>
                     </div>
