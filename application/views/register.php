571437b4dd99f38ba46d2e0d832d847ded923067
SOLANKIJAY15 <jaysolanki934@gmail.com>
2022-03-17 16:42:53 +0530

    add files

--- application/views/register.php
+++ application/views/register.php
@@ -65,34 +65,36 @@ include("header.php");
                   </div>
                 </div>
                 
-                    <div class="form-outline"><br>
-                        <label class="form-label">Country</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <select class="select form-control-lg" style="background-color:white; border-color:red;">
-                            <option value="1" disabled>Choose option</option>
-                            <option value="2">Subject 1</option>
-                            <option value="3">Subject 2</option>
-                            <option value="4">Subject 3</option>
-                        </select>
+                <div class="col-md-6 mb-4 pb-2">
+                    <div class="form-group">
+                        <label class="form-label">Country</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <select class="form-control" >
+                                <option value="1" disabled>Choose option</option>
+                                <option value="2">Subject 1</option>
+                                <option value="3">Subject 2</option>
+                                <option value="4">Subject 3</option>
+                            </select>
                     </div>
+                </div>
             </div>
 
 
             <div class="row">
-                <div class="col-md-6 mb-4 pb-2">
-                    <div class="form-outline">
-                        <label class="form-label">State</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <select class="select form-control-lg" style="background-color:white; border-color:red;">
-                            <option value="1" disabled>Choose option</option>
-                            <option value="2">Subject 1</option>
-                            <option value="3">Subject 2</option>
-                            <option value="4">Subject 3</option>
-                        </select>
+            <div class="col-md-6 mb-4 pb-2">
+                    <div class="form-group">
+                        <label class="form-label">State</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <select class="form-control" >
+                                <option value="1" disabled>Choose option</option>
+                                <option value="2">Subject 1</option>
+                                <option value="3">Subject 2</option>
+                                <option value="4">Subject 3</option>
+                            </select>
                     </div>
                 </div>
                 <div class="col-md-6 mb-4 pb-2">
-                    <div class="form-outline">
+                    <div class="form-group">
                         <label class="form-label">City</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            <select class="select form-control-lg" style="background-color:white; border-color:red;"    >
+                            <select class="form-control" >
                                 <option value="1" disabled>Choose option</option>
                                 <option value="2">Subject 1</option>
                                 <option value="3">Subject 2</option>
@@ -117,10 +119,20 @@ include("header.php");
                 </div>
             </div>
 
-            
-              <div class="mt-4 pt-2">
+           
+              <!-- <div class="mt-4 pt-2">
                 <input class="btn btn-primary btn-lg" type="submit" value="Submit" />
-              </div>
+              </div> -->
+
+                <button type="submit" style="
+  background-color: red;
+  color: white;
+  padding: 15px 20px;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  opacity: 0.9;
+" class="btn">Submit</button>
 
             </form>
           </div>
@@ -129,3 +141,7 @@ include("header.php");
     </div>
   </div>
 </section>
+
+<?php
+    include("footer.php");
+?>
